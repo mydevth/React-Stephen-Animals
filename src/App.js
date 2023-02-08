59639fc157c813bef9e0db5eb6e4f5d19d870310
Mydevth <mydevth@chonburi.biz>
2023-02-08 17:16:43 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AnimalShow from "./AnimalShow";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleClick}> Add Animal </button>
+      <p>Number of animals : {count} </p>
+      <AnimalShow />
+    </>
   );
-}
+};
 
 export default App;
